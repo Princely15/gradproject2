dedfd5a13a4f39ad994b5ff7c6ef82cd543af7a4
Richmond <richmondoginni@rocketmail.com>
2026-02-24 12:04:05 -0600

    Made text file and js file with required inputting

---
+++ pro.js
@@ -0,0 +1,10 @@
+let partyLocation = "GARDEN"; // String (Message Might Change)
+
+const shiftValue = 3; // Number (Shift Value Remains Constant)
+Number.isInteger(shiftValue);
+
+let encryptedMessage = ""; // String (Changed when encoded)
+
+let decryptedMessage = ""; // String (Changed When Decoded)
+
+let isPartySafe = false; // Boolean 
