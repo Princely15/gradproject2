d87dac25ab965d5d4893f5e0fc571bf00baa1e25
Princely15 <richmondoginni@rocketmail.com>
2026-02-24 22:46:20 -0600

    Updated Cipher

--- pro.js
+++ pro.js
@@ -1,11 +1,29 @@
 let partyLocation = "GARDEN"; // String (Message Might Change)
 
-const shiftValue = 3; // Number (Shift Value Remains Constant)
 Number.isInteger(shiftValue);
 
-let encryptedMessage = ""; // String (Changed when encoded)
+const encryptedMessage = "EUXWXV"; // String (Changed when encoded)
 
 let decryptedMessage = ""; // String (Changed When Decoded)
 
 let isPartySafe = false; // Boolean 
 // New Changes //
+
+const friend = "BRUTUS"
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const firstLetter = friend[0]
+const index = alphabet.indexOf(firstLetter.toLowerCase());
+// Q1 = Zero Based indexing
+
+const alphabetLength = alphabet.length;
+//Q2 = Modulus (%) brings us back to the start
+const newIndex = (index + shiftValue) % alphabetLength;
+const encryptedFirstletter = alphabet[newIndex];
+const teaserMessage = encryptedMessage.slice(0,3);
+
+const randomDecimal = Math.random();
+const range = 33 - 3 + 1; // 31
+const randomInRange = randomDecimal * range;
+const randomInt = Math.floor(randomInRange);
+const shiftValue = randomInt + 3;
